fdbf8f20d29ea7c838272e1897d57048df96dfce
ismail118 <ismailalfiyasin6@gmail.com>
2022-03-10 04:10:57 +0700

    operasi perbandingan

---
+++ 11-operasi-perbandingan/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama1 = "Eko"
+	var nama2 = "Eko"
+
+	var result bool = nama1 == nama2
+
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
