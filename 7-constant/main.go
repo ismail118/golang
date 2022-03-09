e8473b563960a33d11c01f9c509ae99120b55054
ismail118 <ismailalfiyasin6@gmail.com>
2022-03-10 02:39:55 +0700

    constant

--- 7-constant/main.go
+++ 7-constant/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	const firstName string = "Eko"
 	const lastName = "Khannedy"
 	const value = 1000
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+
+	// multiple deklarasi constant
+
+	const (
+		name     = "ismail"
+		fullName = "ismail alfiyasin"
+		age      = 24
+	)
+
+	fmt.Println(name, fullName, age)
 }
