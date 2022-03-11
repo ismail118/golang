cc79743424e3eca92f7bdb1c98121ec9029d40a1
ismail118 <ismailalfiyasin6@gmail.com>
2022-03-11 15:57:32 +0700

    function as parameter

---
+++ 27-function-as-parameter/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Filter func(string) string
+
+func sayHelloWithFilter(name string, filter func(string) string) {
+	nameFiltered := filter(name)
+	fmt.Println("Hello", nameFiltered)
+}
+
+func sayHelloWithFilter2(name string, filter Filter) {
+	nameFiltered := filter(name)
+	fmt.Println("Hello", nameFiltered)
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "...."
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHelloWithFilter("Eko", spamFilter)
+	sayHelloWithFilter("Anjing", spamFilter)
+
+	sayHelloWithFilter2("Anjing", spamFilter)
+}
