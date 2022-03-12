340ad386346e0c54e0a01db3bb279199963352f9
ismail118 <ismailalfiyasin6@gmail.com>
2022-03-12 13:48:00 +0700

    interface

---
+++ 35-interface/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+// implementasi interface HasName
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	person := Person{Name: "Eko"}
+	SayHello(person)
+
+	animal := Animal{Name: "Burung"}
+	SayHello(animal)
+
+}
