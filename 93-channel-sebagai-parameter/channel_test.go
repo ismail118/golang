a3955dcfd5eefe1ef9d8eab9a66c90bafa588b0b
Ismail Alfiyasin <ismailalfiyasin6@gmail.com>
2022-03-26 21:44:12 +0700

    93-channel-sebagai-parameter

--- 93-channel-sebagai-parameter/channel_test.go
+++ 93-channel-sebagai-parameter/channel_test.go
@@ -18,4 +18,23 @@ func TestCreateChannel(t *testing.T) {
 	data := <-channel
 	fmt.Println(data)
 	close(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "EKo Kurniawan Khannedy"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	close(channel)
+
+	time.Sleep(5 * time.Second)
 }
