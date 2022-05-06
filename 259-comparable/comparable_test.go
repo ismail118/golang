3f347495ba3db3cee91daf2eff4657bcb6c35dff
Ismail Alfiyasin <ismailalfiyasin6@gmail.com>
2022-05-06 09:19:13 +0700

    259 comparable

---
+++ 259-comparable/comparable_test.go
@@ -0,0 +1,19 @@
+package _59_comparable
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func IsSame[T comparable](value1, value2 T) bool {
+	if value1 == value2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func TestIsSame(t *testing.T) {
+	assert.Equal(t, true, IsSame[string]("eko", "eko"))
+	assert.Equal(t, true, IsSame[int](100, 100))
+}
