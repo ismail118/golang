c9642050b0555b42904604ab434d908920e35906
Ismail Alfiyasin <ismailalfiyasin6@gmail.com>
2022-05-06 10:50:54 +0700

    262 type approximation

---
+++ 262-type-approximation/type_approximation_test.go
@@ -0,0 +1,27 @@
+package _62_type_approximation
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Age int
+
+type Number interface {
+	~int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](first, second T) T {
+	if first < second {
+		return first
+	} else {
+		return second
+	}
+}
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, int(100), Min[int](100, 200))
+	assert.Equal(t, int64(100), Min[int64](100, 200))
+	assert.Equal(t, float64(100.0), Min[float64](100.0, 200.0))
+	assert.Equal(t, Age(100), Min[Age](Age(100), Age(200)))
+}
